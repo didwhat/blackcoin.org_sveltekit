f95b4a23b0471b099ed4b1a3d30ece00de940b86
Daniel Clough <danielclough@users.noreply.github.com>
2022-11-18 17:55:03 -0800

    display none

--- src/lib/components/header/hidden-links.svelte
+++ src/lib/components/header/hidden-links.svelte
@@ -2,6 +2,6 @@
     export let locales
 </script>
 {#each locales as locale}
-	<a style="visability:hidden;" href="/{locale.code}/donations">/{locale.code}/donations</a>
-	<a style="visability:hidden;" href="/{locale.code}/faq">/{locale.code}/faq</a>
+	<a style="display:none;" href="/{locale.code}/donations">/{locale.code}/donations</a>
+	<a style="display:none;" href="/{locale.code}/faq">/{locale.code}/faq</a>
 {/each}
